0d469da90a6ea6cc56073c86aa3a7cff22bc59ed
mattia <m.vitali@k3progetti.it>
2025-09-30 16:04:40 +0200

    feat: read and send appSecret

--- src/Service/ComposerInfoService.php
+++ src/Service/ComposerInfoService.php
@@ -3,25 +3,36 @@
 namespace K3Progetti\SoftwareVersionBundle\Service;
 
 use Composer\InstalledVersions;
+use JsonException;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Throwable;
 
 final class ComposerInfoService
 {
-    public function __construct(private KernelInterface $kernel) {}
+    public function __construct(
+        private KernelInterface       $kernel,
+        private ParameterBagInterface $params
+    )
+    {
+    }
 
+    /**
+     * @return array
+     * @throws JsonException
+     */
     public function getInfo(): array
     {
-        $path = $this->kernel->getProjectDir().'/composer.json';
+        $path = $this->kernel->getProjectDir() . '/composer.json';
 
         $name = $description = $phpReq = $symfonyReq = $symfonyInstalled = null;
 
         if (is_file($path)) {
-            $json = json_decode((string) file_get_contents($path), true, 512, JSON_THROW_ON_ERROR);
-            $name        = $json['name']        ?? null;
+            $json = json_decode((string)file_get_contents($path), true, 512, JSON_THROW_ON_ERROR);
+            $name = $json['name'] ?? null;
             $description = $json['description'] ?? null;
-            $phpReq      = $json['require']['php'] ?? null;
-            $symfonyReq  = $json['extra']['symfony']['require']
+            $phpReq = $json['require']['php'] ?? null;
+            $symfonyReq = $json['extra']['symfony']['require']
                 ?? $json['require']['symfony/framework-bundle']
                 ?? null;
         }
@@ -34,16 +45,22 @@ final class ComposerInfoService
                 } elseif (InstalledVersions::isInstalled('symfony/http-kernel')) {
                     $symfonyInstalled = InstalledVersions::getPrettyVersion('symfony/http-kernel');
                 }
-            } catch (Throwable) {}
+            } catch (Throwable) {
+            }
         }
 
+
+        // Leggi APP_SECRET
+        $appSecret = $this->params->get('env(APP_SECRET)');
+
         return [
-            'name' => $name,
+            'name' => str_replace('template/', '', $name),
             'description' => $description,
             'phpRequirement' => $phpReq,
             'symfonyRequirement' => $symfonyReq,
             'symfonyInstalledVersion' => $symfonyInstalled,
             'phpInstalledVersion' => PHP_VERSION,
+            'appSecret' => $appSecret,
         ];
     }
 }
